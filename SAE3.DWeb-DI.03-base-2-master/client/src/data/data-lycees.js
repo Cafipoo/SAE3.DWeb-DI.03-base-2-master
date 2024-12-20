3cc400080762547b3e2d51e2c3c85d8cd238bfde
François-Pierre Lajudie <129268455+Cafipoo@users.noreply.github.com>
2024-12-20 14:52:34 +0100

    Update data-lycees.js

--- SAE3.DWeb-DI.03-base-2-master/client/src/data/data-lycees.js
+++ SAE3.DWeb-DI.03-base-2-master/client/src/data/data-lycees.js
@@ -1,11 +1,49 @@
-import { Candidats } from "./data-candidats.js";
+let data = await fetch("./src/data/json/lycees.json");
+data = await data.json();
 
-let dataPromise = fetch("./src/data/json/lycees.json").then(response => response.json());
+let Lycees = {}
 
-let Lycees = {};
+Lycees.getAll = function(){ 
+    return data.slice(1).map(lycee => ({
+            appellation_officielle: lycee.appellation_officielle,
+            longitude: lycee.longitude,
+            latitude: lycee.latitude
+        })
+    );
+}
+Lycees.getEcole = function(candidats){
+    let result = [];
+    let sortedData = data.sort((a, b) => a.numero_uai.localeCompare(b.numero_uai));
+    
+    function binarySearch(arr, x, key) {
+        let low = 0, high = arr.length - 1;
+        while (low <= high) {
+            let mid = Math.floor((low + high) / 2);
+            if (arr[mid][key] === x) return arr[mid];
+            else if (arr[mid][key] < x) low = mid + 1;
+            else high = mid - 1;
+        }
+        return null;
 
-Lycees.getLyceecandidat = async function(candidats) {
-    let data = await dataPromise;
+    }
+    let totalCandidats = 0;
+    for (let candidat of candidats) {
+        let lycee = binarySearch(sortedData, candidat.ecole, 'numero_uai') || binarySearch(sortedData, candidat.ecole, 'appellation_officielle');
+        if (lycee && lycee.longitude && lycee.latitude) {
+            result.push({
+                appellation_officielle: lycee.appellation_officielle,
+                longitude: lycee.longitude,
+                latitude: lycee.latitude,
+                nbCandidat: candidat.nbCandidat
+            });
+            totalCandidats += candidat.nbCandidat;
+        }  
+    }
+    console.log("Total candidats:", totalCandidats);
+    return result;
+}
+Lycees.getLyceecandidat = function(candidats){
+    let result = [];
     let sortedData = data.sort((a, b) => a.numero_uai.localeCompare(b.numero_uai));
 
     function binarySearch(arr, x, key) {
@@ -20,25 +58,24 @@ Lycees.getLyceecandidat = async function(candidats) {
     }
 
     let totalCandidats = 0;
-    let lyceesMap = new Map();
+    let lyceesMap = new Map(); // Utilisation d'une Map pour regrouper les données par lycée
 
     for (let candidat of candidats) {
         let lycee = binarySearch(sortedData, candidat.ecole, 'numero_uai');
 
         if (lycee && lycee.longitude && lycee.latitude) {
-            let key = lycee.numero_uai;
+            let key = lycee.numero_uai; 
             if (!lyceesMap.has(key)) {
                 lyceesMap.set(key, {
                     appellation_officielle: lycee.appellation_officielle,
                     longitude: lycee.longitude,
                     latitude: lycee.latitude,
                     code_postal_uai: lycee.code_postal_uai.slice(0, -3) + '000',
-                    series: []
+                    series: [] 
                 });
             }
-            let lyceeData = lyceesMap.get(key);
             for (let serie of candidat.series) {
-                lyceeData.series.push({
+                lyceesMap.get(key).series.push({
                     SerieDiplomeCode: serie.SerieDiplomeCode,
                     nbCandidat: serie.nbCandidats
                 });
@@ -47,7 +84,7 @@ Lycees.getLyceecandidat = async function(candidats) {
         }
     }
 
-    let result = Array.from(lyceesMap.values());
+    result = Array.from(lyceesMap.values());
     result.sort((a, b) => a.code_postal_uai.localeCompare(b.code_postal_uai));
     return result;
 }
